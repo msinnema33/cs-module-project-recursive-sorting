8d11d965335abf11ef0b18d0a9ba52da9a72c703
Mike Sinnema <msinnema33@gmail.com>
2020-06-15 12:39:09 -0700

    recursive binary search working -- agnostic not yet

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,14 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+   if start > end: # check if list is sorted ascending.
+        return -1
+   mid = start + end //2 # get middle floor
+   if target == arr[mid]:
+       return mid
+   elif target < arr[mid]:
+        return binary_search(arr, target, start, mid -1)
+   else:
+        return binary_search(arr, target, mid +1, end)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +19,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
